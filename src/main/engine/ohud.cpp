d27d18a90825d3fe8cc44ed3f7b4b2238f3bedc5
Simon Prickett <simon.prickett@grafana.com>
2025-11-18 21:56:00 +0000

    Added log that uses timestamp fn.

--- src/main/engine/ohud.cpp
+++ src/main/engine/ohud.cpp
@@ -275,7 +275,7 @@ void OHud::draw_score(uint32_t addr, const uint32_t score, uint8_t font)
 
     video.write_text16(&addr, digits[7] + BASE); // Always draw last digit
     if (outrun.game_state == GS_INGAME) {
-        std::cout << "SIMON SCORE: " << (int)digits[0] << (int)digits[1] << (int)digits[2] << (int)digits[3] << (int)digits[4] << (int)digits[5] << (int)digits[6] << (int)digits[7] << std::endl;
+        std::cout << Utils::get_timestamp_ms() << ": " << "SIMON SCORE: " << (int)digits[0] << (int)digits[1] << (int)digits[2] << (int)digits[3] << (int)digits[4] << (int)digits[5] << (int)digits[6] << (int)digits[7] << std::endl;
     }
     delete[] digits;
 }
@@ -732,7 +732,7 @@ void OHud::blit_text_new(uint16_t x, uint16_t y, const char* text, uint16_t pal)
         // Convert lowercase characters to uppercase
         if (c >= 'a' && c <= 'z')
             c -= 0x20;
-        else if (c == '�')
+        else if (c == '�')  // SIMON TODO THIS GETS MANGLED WITH THE PI, FIX IT AT THE END
             c = 0x10;
         else if (c == '-')
             c = 0x2d;
